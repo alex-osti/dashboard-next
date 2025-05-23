1d9cb7c7d366fd305e1c7a2cfa948e216e171de1
Alex Osti <alex-osti@users.noreply.github.com>
2025-05-23 04:03:56 +0200

    fix: V33

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -291,7 +291,7 @@ export default function DashboardPage() {
                              <>
                                 <h1>
                                     <span className="dashicons dashicons-admin-users"></span>
-                                    Welcome, <span> {escapeHtml(firstName)}{companyName !== 'Your Company' ? ` from ${escapeHtml(companyName)}` : ''}</span>!
+                                    Welcome,&nbsp;<span>{escapeHtml(firstName)}{companyName !== 'Your Company' ? ` from ${escapeHtml(companyName)}` : ''}</span>!
                                 </h1>
                                 <p className="lead">This dashboard highlights how MakerToo can assist {escapeHtml(companyName)} in <strong>{escapeHtml(fromAbstract)}</strong>.</p>
                                 <p>Explore below for tailored insights and our detailed research.</p>
